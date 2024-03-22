dc7be3ffdcbd1c151adbb146a694fec628a46315
Andrii Pakhomov <pahomeathome@gmail.com>
2024-03-22 19:24:35 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { CircularProgress, ThemeProvider } from '@mui/material';
@@ -6,7 +7,7 @@ import { CircularProgress, ThemeProvider } from '@mui/material';
 import ProtectedRoute from './functions/ProtectedRoute';
 import ClientsPage from './pages/ClientsPage';
 import LoginPage from './pages/LoginPage';
-import NotFoundPage from './pages/NotFoundPage';
+// import NotFoundPage from './pages/NotFoundPage';
 import theme from './themes/theme';
 
 const queryClient = new QueryClient();
@@ -26,8 +27,8 @@ const router = createBrowserRouter(
       element: <LoginPage />,
     },
     {
-      path: '/*',
-      element: <NotFoundPage />,
+      path: '*',
+      element: <Navigate to="/login" replace={true} />,
     },
   ],
   { basename: '/med-tax-test' },
